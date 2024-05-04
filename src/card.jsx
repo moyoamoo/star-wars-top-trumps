6c9724e729ca6ee2fb31c8443eacde1ecb09cc45
Moyo A-P <moyo.amoo@hotmail.com>
2024-05-04 13:45:17 +0100

    moved message in redux

--- src/card.jsx
+++ src/card.jsx
@@ -1,12 +1,28 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getCharacter } from "./starshipApiRequest";
-import { selectComputerChoice, selectStarship } from "./redux/cardSlice";
+import {
+  selectComputerChoice,
+  selectStarship,
+  selectMessage,
+  setMessage,
+} from "./redux/cardSlice";
 import { useState } from "react";
 
 const Card = () => {
-  const [message, setMessage] = useState(undefined);
   const starship = useSelector(selectStarship);
   const computerChoice = useSelector(selectComputerChoice);
+  const message = useSelector(selectMessage);
+  const dispatch = useDispatch();
+
+  const playTurn = (userValue, computerValue) => {
+    if (Number(computerValue) === Number(userValue)) {
+      return dispatch(setMessage("It's a tie"));
+    } else if (Number(computerValue) > Number(userValue)) {
+      return dispatch(setMessage("Computer won"));
+    } else if (Number(computerChoice) < Number(userValue)) {
+      return dispatch(setMessage("You won"));
+    }
+  };
   return (
     <>
       {starship && (
@@ -16,28 +32,42 @@ const Card = () => {
           <button
             onClick={() => {
               if (computerChoice) {
-                if (Number(computerChoice.length) === Number(starship.length)) {
-                  console.log(computerChoice.length);
-                  setMessage("It's a tie");
-                } else if (
-                  Number(computerChoice.length) > Number(starship.length)
-                ) {
-                  console.log(computerChoice.length);
-                  setMessage("Computer won");
-                } else if (
-                  Number(computerChoice.length) < Number(starship.length)
-                ) {
-                  console.log(computerChoice.length);
-                  setMessage("You won");
-                }
+                playTurn(starship.length, computerChoice.length);
               }
             }}
           >
             <p>Length: {starship.length}</p>
           </button>
-          <p>Max Atmosphering Speed: {starship.max_atmosphering_speed}</p>
-          <p>Crew: {starship.crew}</p>
-          <p>Passengers: {starship.passengers}</p>
+          <button
+            onClick={() => {
+              if (computerChoice) {
+                playTurn(
+                  starship.max_atmosphering_speed,
+                  computerChoice.max_atmosphering_speed
+                );
+              }
+            }}
+          >
+            <p>Max Atmosphering Speed: {starship.max_atmosphering_speed}</p>
+          </button>
+          <button
+            onClick={() => {
+              if (computerChoice) {
+                playTurn(starship.crew, computerChoice.crew);
+              }
+            }}
+          >
+            <p>Crew: {starship.crew}</p>
+          </button>
+          <button
+            onClick={() => {
+              if (computerChoice) {
+                playTurn(starship.passengers, computerChoice.passengers);
+              }
+            }}
+          >
+            <p>Passengers: {starship.passengers}</p>
+          </button>
         </div>
       )}
 
--- src/redux/cardSlice.js
+++ src/redux/cardSlice.js
@@ -6,8 +6,10 @@ const initialState = {
 export const cardSlice = createSlice({
   name: "counter",
   initialState,
+  message: "",
   starship: {},
   computerChoice: {},
+
   reducers: {
     setStarship: (state, { payload }) => {
       state.starship = payload;
@@ -16,11 +18,17 @@ export const cardSlice = createSlice({
     setComputerChoice: (state, { payload }) => {
       state.computerChoice = payload;
     },
+
+    setMessage: (state, { payload }) => {
+      console.log(payload)
+      state.message = payload;
+    },
   },
 });
 
-export const { setStarship, setComputerChoice } = cardSlice.actions;
+export const { setStarship, setComputerChoice, setMessage } = cardSlice.actions;
 
 export const selectStarship = (state) => state.card.starship;
 export const selectComputerChoice = (state) => state.card.computerChoice;
+export const selectMessage = (state) => state.card.message;
 export default cardSlice.reducer;
