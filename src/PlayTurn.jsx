96fa7d7e337ec17b7325cb655cdaef636cbebda8
Moyo A-P <moyo.amoo@hotmail.com>
2024-05-04 14:02:14 +0100

    save

--- src/PlayTurn.jsx
+++ src/PlayTurn.jsx
@@ -1,12 +1,16 @@
+import { useDispatch } from "react-redux";
 import { getCharacter, getComputerChoice } from "./starshipApiRequest";
+import { setMessage } from "./redux/cardSlice";
 
 const PlayTurn = () => {
+  const dispatch = useDispatch();
   return (
     <>
       <button
         onClick={() => {
           getCharacter();
-          getComputerChoice()
+          getComputerChoice();
+          dispatch(setMessage(""));
         }}
       >
         Play Turn
